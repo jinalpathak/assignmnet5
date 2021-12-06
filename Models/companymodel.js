7da930ab63cecf1291a881ff9e6f9116ae16e7f9
jinalpathak <91406816+jinalpathak@users.noreply.github.com>
2021-12-06 15:33:07 +0000

    done

--- Models/companymodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-const companySchema=mongoose.Schema({
-    companyId : String,
-    name : String,
-    productId : [String]
-})
-
-const companyModel=mongoose.model("company",companySchema,"company");
-module.exports=companyModel;
